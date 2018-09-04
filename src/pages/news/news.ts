21f0015a0959815b737b1d7401c6b6199f966e36
Alberto Martinez <betoeuge@gmail.com>
2018-09-03 23:19:55 -0400

    logos

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,15 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { BcnationRestProvider } from '../../providers/bcnation-rest/bcnation-rest';
 
-/**
- * Generated class for the NewsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-news', 
   templateUrl: 'news.html',
--- src/pages/sponsors/sponsors.ts
+++ src/pages/sponsors/sponsors.ts
@@ -1,15 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { BcnationRestProvider } from '../../providers/bcnation-rest/bcnation-rest';
 
-/**
- * Generated class for the SponsorsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-sponsors',
   templateUrl: 'sponsors.html',
